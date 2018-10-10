8f058bd6b5e09d2cd6da8d0b842607339ecf7989
BrunoVinicius <brunoviniciustica@gmail.com>
2018-10-09 21:01:49 -0300

    Comitt classe Fachada

--- src/burgueriarafael/fachada/ClienteFachada.java
+++ src/burgueriarafael/fachada/ClienteFachada.java
@@ -5,10 +5,44 @@
  */
 package burgueriarafael.fachada;
 
+import burgueriarafael.basica.Cliente;
+import burgueriarafael.negocio.ClienteNegocio;
+import java.util.List;
+
 /**
  *
  * @author BrunoTiCaVini
  */
 public class ClienteFachada {
     
+    ClienteNegocio clientenegocio = null;
+    
+    
+    public boolean salvaCliente(Cliente cliente) throws Exception {
+     clientenegocio = new ClienteNegocio();
+     clientenegocio.insert(cliente);
+     
+     return true;
+    }
+    
+    public boolean alterarCliente(Cliente cliente) throws Exception {
+    clientenegocio = new ClienteNegocio();
+    clientenegocio.update(cliente);
+    
+    return true;
+    }
+    
+    public boolean deletarCliente(Cliente cliente) throws Exception {
+    clientenegocio = new ClienteNegocio();
+    clientenegocio.delete(cliente);
+    
+    return true;
+    }
+    
+    public List<Cliente> consultarCliente() throws Exception {
+    
+        clientenegocio = new ClienteNegocio();
+        return clientenegocio.select();
+        
+    }
 }
